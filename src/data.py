ca0ef277cb494385bbab1b0587872637334de0ab
daliondev <d4lion.dev@gmail.com>
2023-01-11 13:27:49 -0500

    feat(add): data reader

--- src/data.py
+++ src/data.py
@@ -1,10 +1,18 @@
 import os
 
-
 class data:
-    def __init__(self):
-        pass
+
+    def __init__(self) -> None:
+        self.cwd = os.getcwd()
+        self.filesDirectory = f'{self.cwd}/data'
+        self.files = os.listdir(self.filesDirectory)
 
 
+    def read_content(self) -> list[str]:
+        for file in self.files:
+            with open(f'{self.filesDirectory}/{file}') as f:
+                self.content = f.readlines()
+                f.close()
 
+        return self.content
 
