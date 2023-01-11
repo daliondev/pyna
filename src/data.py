14b3fc6f7fc4d82d1785ccbfda95e7d70ab61dfa
daliondev <d4lion.dev@gmail.com>
2023-01-11 13:29:12 -0500

    fix(list): fixes the list that stores the content

--- src/data.py
+++ src/data.py
@@ -1,18 +1,28 @@
 import os
 
+
 class data:
 
     def __init__(self) -> None:
+        self.dataContent = []
+
         self.cwd = os.getcwd()
+
+        # Path donde estan los archivos por leer
         self.filesDirectory = f'{self.cwd}/data'
+
+        # Archivos listados
         self.files = os.listdir(self.filesDirectory)
 
 
-    def read_content(self) -> list[str]:
+    def content(self) -> list[str]:
+
+        # Recorremos los nombres de los archivos dados por self.files
         for file in self.files:
+            # Entramos al directorio donde estan los archivos para poder leerlos
             with open(f'{self.filesDirectory}/{file}') as f:
-                self.content = f.readlines()
+                self.dataContent.append(f.readlines())
                 f.close()
 
-        return self.content
+        return self.dataContent
 
